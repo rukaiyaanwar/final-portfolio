1f15961ff39042454eb56df22f223dbca54ea2de
rukaiyaanwar <rukaiyaanwar@users.noreply.github.com>
2022-07-27 16:15:12 +0530

    Theme Color updates

--- js/style_switcher.js
+++ js/style_switcher.js
@@ -13,3 +13,20 @@ window.addEventListener("scroll", ()=>{
         document.querySelector(".style-switcher").classList.remove("open");
     }
 })
+
+
+// Theme colors 
+
+const alternateStyle = document.querySelectorAll(".alternate-style");
+
+
+function setActiveStyle(color) {
+    alternateStyle.forEach(style => {
+        if(color === style.getAttribute("title")) {
+            style.removeAttribute("disabled");
+        }
+        else {
+            style.setAttribute("disabled","true");
+        }
+    })
+}
