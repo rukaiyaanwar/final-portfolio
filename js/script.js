a0662fdfc88f29babf8706e84001cedb83a01200
rukaiyaanwar <rukaiyaanwar@users.noreply.github.com>
2022-07-27 20:14:55 +0530

    JS updates

--- js/script.js
+++ js/script.js
@@ -9,3 +9,31 @@ var Typed = new Typed(".typing", {
   BackSpeed: 60,
   loop: true,
 });
+
+// Aside 
+
+const nav = document.querySelector(".nav"),
+        navList = document.querySelectorAll("li"),
+        totalNavList = navList.length,
+        allSection = document.querySelectorAll(".section"),
+        totalSection = allSection.length;
+        for(let i=0; i < totalNavList; i++) {
+            const a = navList[i].querySelector("a");
+            a.addEventListener("click", function() {
+                for(let j=0; j<totalNavList; j++) {
+                    navList[j].querySelector("a").classList.remove("active");
+                }
+                this.classList.add("active");
+                showSection(this);
+            })
+        }
+
+        function showSection(element) {
+            for(let i = 0; i < totalSection; i++) {
+                allSection[i].classList.remove("active");
+            }
+            const target = element.getAttribute("href").split("#")[1];
+            document.querySelector("#" + target).classList.add("active");
+        }
+
+
