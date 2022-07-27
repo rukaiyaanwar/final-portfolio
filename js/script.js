4b652a5e2aa330882d5a457bdb6e9361d9b71033
rukaiyaanwar <rukaiyaanwar@users.noreply.github.com>
2022-07-27 17:35:24 +0530

    Typed.js updates

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var Typed = new Typed(".typing", {
+  strings: [
+    "Web Developer",
+    "Food Blogger",
+    "Content creator",
+    "Full Stack Developer",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
